19a50acda5ad6a8b835a97c366a2652431fa970a
Crowdbotics <team@crowdbotics.com>
2021-08-05 12:58:00 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "aged_thunder_29506Identifier",
+  url: "https://aged_thunder_29506Identifier.botics.co",
+  api: "https://aged_thunder_29506Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
